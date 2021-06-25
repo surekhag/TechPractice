be99546a467bc738c1766edb299fce1f8be6b500
surekhag <surekha19.mca@gmail.com>
2021-06-25 13:30:14 +0530

    Added nodemon setup n starting routers

---
+++ Routers/index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app= express();
+const port = process.env.port || 4000;
+app.get("/", (req,res)=>{
+res.send("Hii0")
+console.log("listening to you dear", req)
+})
+app.listen(port, ()=>{
+console.log("App started at port : ", port)
+})
